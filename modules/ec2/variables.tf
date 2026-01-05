19b0b9fa764a3d0da50218015890a3e8df4f64bc
devopslab-admin <hdevops2025@gmail.com>
2026-01-05 22:13:50 +0530

    Attach IAM instance profile to validation EC2 using Terraform

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,14 +1,20 @@
 variable "subnet_id" {
-  description = "Subnet ID for validation instance"
+  description = "Subnet ID for validation EC2"
   type        = string
 }
 
 variable "security_group_id" {
-  description = "Security group for validation instance"
+  description = "Security group ID for validation EC2"
+  type        = string
+}
+
+variable "iam_instance_profile" {
+  description = "IAM instance profile name"
   type        = string
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "Common resource tags"
+  type        = map(string)
+  default     = {}
 }
